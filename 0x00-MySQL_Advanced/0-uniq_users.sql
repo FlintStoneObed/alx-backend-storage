43d2e286e75edce7accb8b28645d6e121c37427a
FlintStoneObed <141341733+FlintStoneObed@users.noreply.github.com>
2024-08-08 12:16:52 +0000

    Update 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,7 @@
--- Create users table
-
-CREATE TABLE IF NOT EXISTS users (
-  -- Attributes
-  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  name VARCHAR(255) NOT NULL
+-- Creates a table with unique users.
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 );
